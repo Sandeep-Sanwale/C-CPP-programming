2380ec70e34bfdcf3669c29834e96ffa27c303ab
sankyy79 <73604286+sankyy79@users.noreply.github.com>
2020-11-11 16:32:57 +0530

    Update Simple calculator using switch case .c

--- Simple calculator using switch case .c
+++ Simple calculator using switch case .c
@@ -2,7 +2,7 @@
 int main() {
     char operator;
     double first, second;
-    printf("Enter an operator (+, -, *,): ");
+    printf("Enter an operator (+, -, *,/): ");
     scanf("%c", &operator);
     printf("Enter two operands: ");
     scanf("%lf %lf", &first, &second);
