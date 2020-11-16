5cc48cac564d2002e2600304b6124b9b60fb2c72
sankyy79 <73604286+sankyy79@users.noreply.github.com>
2020-11-16 17:54:44 +0530

    pyramid patterns(practising of loops)

---
+++ pattern 15.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+	int i,j,k=0,x;
+	for(i=1;i<=9;i++)
+	{  
+	i<=5?k++:k--;
+	x=1;
+	
+		for(j=1;j<=5;j++)
+		{ 
+		    
+			if(j>=6-k){
+			printf("%d",x);
+			x++;
+		}
+			else
+			printf(" ");
+		}
+		printf("\n");
+	}
+}
