3c3da1f486fe5a67ed502f7dec30e16297f009a2
sankyy79 <73604286+sankyy79@users.noreply.github.com>
2020-11-16 17:57:27 +0530

    pyramid patterns(practising of loops)

---
+++ pattern 21.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+	int i,j,k;
+	
+	for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=8;j++)
+		{
+			if(j>=5-i &&j<=9-i)
+			printf("*");
+			else
+			printf(" ");
+		}
+		printf("\n");
+	}
+}
